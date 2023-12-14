4bef437cdd910ece709989819b93a4e6f18af7b9
J-R-Kenny <153197242+J-R-Kenny@users.noreply.github.com>
2023-12-14 15:24:26 +0000

    added trees functions

--- js/mySketch.js
+++ js/mySketch.js
@@ -5,27 +5,44 @@
 let currentPalette;
 
 function setup() {
-    createCanvas(windowWidth, windowHeight / 2);
-    currentPalette = randomPalette();
-    noStroke();
-    background("white");
-    noLoop();
+  createCanvas(windowWidth, windowHeight / 2);
+  currentPalette = randomPalette();
+  noStroke();
+  background("black");
+  noLoop();
 }
 
 function draw() {
-    
-
-
-
-
-    
-
-
-
-
+  drawTrees(100, 310);
+  drawTrees(150, 300);
+  drawTrees(50, 300);
 }
 
 function mousePressed() {
-    background(0);
-    redraw();
+  background(0);
+  redraw();
+}
+
+function drawTrees(x, y) {
+  push();
+  translate(x, y);
+  fill("saddlebrown");
+  rectMode(CENTER);
+  rect(0, 0, 10, 20);
+  fill("darkgreen");
+  let treeSize = random(-30, -20);
+  drawTreeSection(treeSize);
+  translate(0, -10);
+  drawTreeSection(treeSize + 5);
+  translate(0, -10);
+  drawTreeSection(treeSize + 10);
+  pop();
+}
+
+function drawTreeSection(x) {
+  beginShape();
+  vertex(0, x);
+  vertex(x, 0);
+  vertex(x * -1, 0);
+  endShape(CLOSE);
 }
