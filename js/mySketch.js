8ca0d2bf4085fd2223cfcf6d7638d76c135bd9be
J-R-Kenny <153197242+J-R-Kenny@users.noreply.github.com>
2023-12-14 16:09:08 +0000

    added cabin code

--- js/mySketch.js
+++ js/mySketch.js
@@ -3,6 +3,12 @@
 //It assumes that the file "myPalettes.js" has also been loaded
 
 let currentPalette;
+function preload() {
+  cabinWallSE = loadImage("images/cabinWall_SE.png");
+  cabinSideCenterSW = loadImage("images/cabinSideCenter_SW.png");
+  cabinDoor = loadImage("images/cabinDoor_SW.png");
+  cabinRoofCenter = loadImage("images/cabinRoofCenter_NE.png");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight / 2);
@@ -16,6 +22,7 @@ function draw() {
   drawTrees(100, 310);
   drawTrees(150, 300);
   drawTrees(50, 300);
+  drawCabin(300, 50);
 }
 
 function mousePressed() {
@@ -45,4 +52,17 @@ function drawTreeSection(x) {
   vertex(x, 0);
   vertex(x * -1, 0);
   endShape(CLOSE);
+}
+
+function drawCabin(x, y) {
+  push();
+  translate(x, y);
+  image(cabinWallSE, 130, -30);
+  image(cabinWallSE, 65, 2);
+  image(cabinDoor, 0, 0);
+  image(cabinRoofCenter, 90, -115);
+  image(cabinRoofCenter, 40, -90);
+  cabinSideCenterSW.resize(425, 0);
+  image(cabinSideCenterSW, 45, -25);
+  pop();
 }
